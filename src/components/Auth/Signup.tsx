51abab9442498075db5b8de4a94598fb10480cf3
alonemo <kamishnikov_da@niuitmo.ru>
2023-05-11 04:30:47 +0300

    a

--- src/components/Auth/Signup.tsx
+++ src/components/Auth/Signup.tsx
@@ -24,6 +24,7 @@ const Signup = () => {
   const [errMsg, setErrMsg] = useState('');
 
   const handleChangeFile = async (event: any) => {
+    console.log(process.env.REACT_APP_URL);
     try {
       const formData = new FormData();
       const file = event.target.files[0];
